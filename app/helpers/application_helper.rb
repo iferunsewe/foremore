676c5291c8074b744717422ff2b2c05c870d4374
iferunsewe <iferunsewe@gmail.com>
2022-04-26 12:31:34 +0200

    Fixed navbar link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def active_nav_class(link_path)
-    current_page?(link_path) ? "active" : ""
-  end
-
   def is_deliveries_path_active?
-    current_page?(deliveries_path) || (@delivery && current_page?(delivery_path(@delivery))) || (@delivery && current_page?(edit_delivery_path(@delivery)))
+    if @delivery.nil?
+      current_page?(deliveries_path)
+    elsif @delivery.persisted?
+      current_page?(delivery_path(@delivery)) || current_page?(edit_delivery_path(@delivery))
+    else
+      false
+    end
   end
-      
 end
