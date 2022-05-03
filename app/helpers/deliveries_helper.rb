6fba6759f61e44062a49fb8c08f8eb8d286a6017
iferunsewe <iferunsewe@gmail.com>
2022-05-04 00:12:21 +0200

    Added embed api

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -1,5 +1,10 @@
 module DeliveriesHelper
-  def google_map(center:)
-    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17&key=#{ENV['GOOGLE_API_KEY']}"
+  def google_map(pickup_address:, delivery_address:nil)
+    if delivery_address.nil?
+      url = "https://www.google.com/maps/embed/v1/place?key=#{ENV['GOOGLE_API_KEY']}&q=#{pickup_address}"
+    else
+      url = "https://www.google.com/maps/embed/v1/directions?key=#{ENV['GOOGLE_API_KEY']}&origin=#{pickup_address}&destination=#{delivery_address}&mode=bicycling"
+    end
+    url.gsub(/\s+/, '%20')
   end
 end
