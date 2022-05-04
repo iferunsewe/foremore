243243a81daebc81f160c02200e424ff0e75fb41
iferunsewe <iferunsewe@gmail.com>
2022-05-04 11:34:08 +0200

    Better layout

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -3,7 +3,7 @@ module DeliveriesHelper
     if delivery_address.nil?
       url = "https://www.google.com/maps/embed/v1/place?key=#{ENV['GOOGLE_API_KEY']}&q=#{pickup_address}"
     else
-      url = "https://www.google.com/maps/embed/v1/directions?key=#{ENV['GOOGLE_API_KEY']}&origin=#{pickup_address}&destination=#{delivery_address}&mode=bicycling"
+      url = "https://www.google.com/maps/embed/v1/directions?key=#{ENV['GOOGLE_API_KEY']}&origin=#{pickup_address}&destination=#{delivery_address}&mode=bicycling&zoom=12"
     end
     url.gsub(/\s+/, '%20')
   end
