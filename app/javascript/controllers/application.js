db7eec0502835a3d8b0c82145b17c3ee53c1316a
iferunsewe <iferunsewe@gmail.com>
2022-05-04 17:32:30 +0200

    Updating location after input

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,21 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+function updateMapFromDeliveryAddress() {
+  const deliveryAddressEle = document.getElementById("delivery_delivery_address")
+  if (!deliveryAddressEle) return;
+  deliveryAddressEle.addEventListener("change", function(e) {
+    const deliveryAddress = this.value
+    const map = document.getElementById("google-embed-map")
+    let newMapUrl = new URL(map.src);
+    newMapUrl.searchParams.set('destination', deliveryAddress);
+    newMapUrl.searchParams.delete('zoom')
+    console.log(newMapUrl.toString())
+    map.src = newMapUrl.toString()
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateMapFromDeliveryAddress()
+})
