32f419febdd4c5e21ce542858ef2de4ff0c7c859
iferunsewe <iferunsewe@gmail.com>
2022-05-06 15:26:29 +0200

    Moved host to env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.default_url_options = { host: "www.foremoresupplies.com" }
+  config.action_mailer.default_url_options = { host: ENV["PROD_HOST"] }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
