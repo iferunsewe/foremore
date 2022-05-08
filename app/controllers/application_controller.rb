585c20142374a747f25d94a0426e0f9c2655d67b
iferunsewe <iferunsewe@gmail.com>
2022-05-08 17:48:44 +0200

    Collecting first name and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Helps to give a default location in development
   # See: http://hankstoever.com/posts/11-Pro-Tips-for-Using-Geocoder-with-Rails
   def location
@@ -18,5 +20,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     authenticated_root_path # your path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
