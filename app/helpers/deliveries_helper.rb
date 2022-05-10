fdbcffc3a3cdb9b6eb31c6695905d16945999c30
iferunsewe <iferunsewe@gmail.com>
2022-05-10 20:49:33 +0200

    Delivery page

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -11,4 +11,12 @@ module DeliveriesHelper
   def google_map_fallback_url(pickup_address:)
     "https://www.google.com/maps/embed/v1/place?key=#{ENV['GOOGLE_API_KEY']}&q=#{pickup_address}"
   end
+
+  def iframe_source(delivery:, alt_pickup_address: nil)
+    if delivery.persisted?
+      google_map(pickup_address: delivery.pickup_address.one_line, delivery_address: delivery.delivery_address)
+    else
+      google_map(pickup_address: alt_pickup_address.one_line)
+    end
+  end
 end
