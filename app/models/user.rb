59bf4e4020ef46e263b644d52cb44725718dccd9
iferunsewe <iferunsewe@gmail.com>
2022-05-10 14:48:38 +0200

    Updating user roles on callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,38 @@ class User < ApplicationRecord
 
   enum role: [:normal, :team_admin, :company_admin, :admin]
 
+  after_update :normal!, if: :joining_a_company?
+  after_update :normal!, if: :joining_a_team?
+  after_update :team_admin!, if: :first_team_member?
+  after_update :company_admin!, if: :first_company_member?
+
   def guest?
     persisted?
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def joining_a_team?
+    return false if saved_change_to_team_id.nil?
+    saved_change_to_team_id[0].nil?
+  end
+
+  def joining_a_company?
+    return false if saved_change_to_company_id.nil?
+    saved_change_to_company_id[0].nil?
+  end
+
+  def first_team_member?
+    return false if saved_change_to_team_id.nil?
+    team&.users&.count == 1
+  end
+
+  def first_company_member?
+    return false if saved_change_to_company_id.nil?
+    company&.users&.count == 1
   end
 end
