82f9acc24f62ceb270429b800a266949b9c335ff
iferunsewe <iferunsewe@gmail.com>
2022-07-12 21:21:41 +0200

    Adding and removing now working

--- app/controllers/delivery_items_controller.rb
+++ app/controllers/delivery_items_controller.rb
@@ -1,23 +1,30 @@
 class DeliveryItemsController < ApplicationController
   def create
-    @delivery_item = DeliveryItem.new(delivery_item_params.merge({cart_id: current_user.cart.id}))
-    if @delivery_item.save
-      flash[:notice] = "The product was added to your delivery"
-      redirect_to new_delivery_path
+    if found_or_created_delivery_item
+      @delivery_items = current_cart.delivery_items.where(active: true)
+      respond_to do |format|
+        format.turbo_stream { render "deliveries/new", notice: "Added to your delivery" }
+      end
     else
-      flash[:alert] = "The product could not be added to your delivery"
-      redirect_to new_delivery_path
+      @delivery_items = current_cart.delivery_items.where(active: true)
+      respond_to do |format|
+        format.turbo_stream { render "deliveries/new", alert: "Something went wrong when adding the product to your delivery" }
+      end
     end
   end
 
   def update
     @delivery_item = DeliveryItem.find(params[:id])
     if @delivery_item.update(delivery_item_params)
-      flash[:notice] = "The product was updated"
-      redirect_to new_delivery_path
+      @delivery_items = current_cart.delivery_items.where(active: true)
+      respond_to do |format|
+        format.turbo_stream { render "deliveries/new", notice: "Delivery updated" }
+      end
     else
-      flash[:alert] = "The product could not be updated"
-      redirect_to new_delivery_path
+      @delivery_items = current_cart.delivery_items.where(active: true)
+      respond_to do |format|
+        format.turbo_stream { render "deliveries/new", alert: "Something went wrong when updating your delivery" }
+      end
     end
   end
 
@@ -25,5 +32,15 @@ class DeliveryItemsController < ApplicationController
 
   def delivery_item_params
     params.require(:delivery_item).permit(:product_id, :quantity, :active)
+  end
+
+  def found_or_created_delivery_item
+    if @delivery_item = DeliveryItem.find_by({product_id: delivery_item_params[:product_id], cart_id: current_cart.id, active: true})
+      @delivery_item.update(quantity: @delivery_item.quantity + delivery_item_params[:quantity].to_i)
+    elsif @delivery_item = DeliveryItem.find_by({product_id: delivery_item_params[:product_id], cart_id: current_cart.id, active: false})
+      @delivery_item.update(quantity: delivery_item_params[:quantity].to_i, active: true)
+    else
+      @delivery_item = DeliveryItem.create(delivery_item_params.merge({cart_id: current_cart.id}))
+    end
   end
 end
